6a75c5f40ed1e76052347fef304c790d184c3cda
wraymo <raywangv@gmail.com>
2024-08-23 20:32:50 -0400

    add clp data source support

--- velox/connectors/clp/ClpDataSource.cpp
+++ velox/connectors/clp/ClpDataSource.cpp
@@ -1,1 +1,218 @@
+#include <boost/filesystem.hpp>
+
+#include "velox/connectors/clp/ClpColumnHandle.h"
+#include "velox/connectors/clp/ClpConnectorSplit.h"
 #include "velox/connectors/clp/ClpDataSource.h"
+#include "velox/connectors/clp/ClpTableHandle.h"
+#include "velox/vector/FlatVector.h"
+
+namespace facebook::velox::connector::clp {
+
+ClpDataSource::ClpDataSource(
+    const RowTypePtr& outputType,
+    const std::shared_ptr<connector::ConnectorTableHandle>& tableHandle,
+    const std::unordered_map<
+        std::string,
+        std::shared_ptr<connector::ColumnHandle>>& columnHandles,
+    velox::memory::MemoryPool* pool,
+    std::shared_ptr<const ClpConfig>& clpConfig)
+    : pool_(pool), outputType_(outputType) {
+  executablePath_ = clpConfig->executablePath();
+  VELOX_CHECK(!executablePath_.empty(), "Executable path must be set");
+  polymorphicTypeEnabled_ = clpConfig->polymorphicTypeEnabled();
+  auto archiveRootDir = clpConfig->archiveDir();
+  VELOX_CHECK(!archiveRootDir.empty(), "Archive directory must be set");
+  auto clpTableHandle = std::dynamic_pointer_cast<ClpTableHandle>(tableHandle);
+  auto archiveDir =
+      boost::filesystem::path(archiveRootDir) / clpTableHandle->tableName();
+  archiveDir_ = archiveDir.string();
+  auto query = clpTableHandle->query();
+  if (query && !query->empty()) {
+    kqlQuery_ = *query;
+  } else {
+    kqlQuery_ = "*";
+  }
+
+  auto outputNames = outputType->names();
+  for (size_t i = 0; i < outputNames.size(); ++i) {
+    auto columnHandle = columnHandles.find(outputNames[i]);
+    VELOX_CHECK(
+        columnHandle != columnHandles.end(),
+        "ColumnHandle not found for output name: {}",
+        outputNames[i]);
+    auto clpColumnHandle =
+        std::dynamic_pointer_cast<ClpColumnHandle>(columnHandle->second);
+    VELOX_CHECK_NOT_NULL(
+        clpColumnHandle,
+        "ColumnHandle must be an instance of ClpColumnHandle for output name: {}",
+        outputNames[i]);
+    columnIndices_[clpColumnHandle->columnName()] = i;
+  }
+}
+
+void ClpDataSource::addSplit(std::shared_ptr<ConnectorSplit> split) {
+  auto clpSplit = std::dynamic_pointer_cast<ClpConnectorSplit>(split);
+  auto tableName = clpSplit->tableName();
+  VELOX_CHECK(!tableName.empty(), "Table name must be set");
+  std::vector<std::string> commands = {"s", archiveDir_, kqlQuery_};
+  resultsStream_.clear();
+  boost::process::child search_process(
+      executablePath_, commands, boost::process::std_out > resultsStream_);
+}
+
+std::optional<RowVectorPtr> ClpDataSource::next(
+    uint64_t size,
+    ContinueFuture& future) {
+  std::vector<VectorPtr> vectors;
+  vectors.reserve(outputType_->size());
+  for (const auto& childType : outputType_->children()) {
+    vectors.emplace_back(BaseVector::create(childType, size, pool_));
+  }
+
+  uint64_t localCompletedRows = 0;
+  for (uint64_t i = 0; i < size; ++i) {
+    std::string line;
+    if (std::getline(resultsStream_, line)) {
+      // Parse the line and return the RowVectorPtr
+      simdjson::ondemand::parser parser;
+      auto doc = parser.iterate(line);
+      std::string path;
+      parseJsonLine(doc, path, vectors, i);
+      localCompletedRows++;
+      completedBytes_ += line.size();
+    } else {
+      // No more data to read
+      break;
+    }
+  }
+  if (localCompletedRows == 0) {
+    return std::nullopt;
+  }
+  completedRows_ += localCompletedRows;
+  return std::make_shared<RowVector>(
+      pool_, outputType_, BufferPtr(), localCompletedRows, std::move(vectors));
+}
+
+void ClpDataSource::parseJsonLine(
+    simdjson::ondemand::value element,
+    std::string& path,
+    std::vector<VectorPtr>& vectors,
+    uint64_t index) {
+  // Parse the json element and populate the vectors
+  switch (element.type()) {
+    case simdjson::ondemand::json_type::object:
+      for (auto field : element.get_object()) {
+        std::string_view key = field.unescaped_key();
+        std::string newPath =
+            path.empty() ? std::string(key) : path + "." + std::string(key);
+        parseJsonLine(field.value(), newPath, vectors, index);
+      }
+      break;
+    case simdjson::ondemand::json_type::string: {
+      if (auto iter = columnIndices_.find(path); iter != columnIndices_.end()) {
+        vectors[iter->second]->asFlatVector<StringView>()->set(
+            index, StringView(element.get_string().value()));
+      } else if (polymorphicTypeEnabled_) {
+        auto typedPath = getTypedColumnName(path, "varchar");
+        if (iter = columnIndices_.find(typedPath);
+            iter != columnIndices_.end()) {
+          vectors[iter->second]->asFlatVector<StringView>()->set(
+              index, StringView(element.get_string().value()));
+        }
+      }
+      break;
+    }
+    case simdjson::ondemand::json_type::number: {
+      simdjson::ondemand::number elementNumber = element.get_number();
+      if (elementNumber.is_double()) {
+        if (auto iter = columnIndices_.find(path);
+            iter != columnIndices_.end()) {
+          vectors[iter->second]->asFlatVector<double>()->set(
+              index, elementNumber.get_double());
+        } else if (polymorphicTypeEnabled_) {
+          auto typedPath = getTypedColumnName(path, "double");
+          if (iter = columnIndices_.find(typedPath);
+              iter != columnIndices_.end()) {
+            vectors[iter->second]->asFlatVector<double>()->set(
+                index, elementNumber.get_double());
+          }
+        }
+      } else {
+        if (auto iter = columnIndices_.find(path);
+            iter != columnIndices_.end()) {
+          vectors[iter->second]->asFlatVector<int64_t>()->set(
+              index, elementNumber.get_int64());
+        } else if (polymorphicTypeEnabled_) {
+          auto typedPath = getTypedColumnName(path, "bigint");
+          if (iter = columnIndices_.find(typedPath);
+              iter != columnIndices_.end()) {
+            vectors[iter->second]->asFlatVector<int64_t>()->set(
+                index, elementNumber.get_int64());
+          }
+        }
+      }
+      break;
+    }
+    case simdjson::ondemand::json_type::boolean: {
+      if (auto iter = columnIndices_.find(path); iter != columnIndices_.end()) {
+        vectors[iter->second]->asFlatVector<bool>()->set(
+            index, element.get_bool());
+      } else if (polymorphicTypeEnabled_) {
+        auto typedPath = getTypedColumnName(path, "boolean");
+        if (iter = columnIndices_.find(typedPath);
+            iter != columnIndices_.end()) {
+          vectors[iter->second]->asFlatVector<bool>()->set(
+              index, element.get_bool());
+        }
+      }
+      break;
+    }
+    case simdjson::ondemand::json_type::null:
+      if (auto iter = columnIndices_.find(path); iter != columnIndices_.end()) {
+        vectors[iter->second]->asFlatVector<StringView>()->setNull(index, true);
+      } else if (polymorphicTypeEnabled_) {
+        auto typedPath = getTypedColumnName(path, "varchar");
+        if (iter = columnIndices_.find(typedPath);
+            iter != columnIndices_.end()) {
+          vectors[iter->second]->asFlatVector<StringView>()->setNull(
+              index, true);
+        }
+      }
+      break;
+    case simdjson::ondemand::json_type::array: {
+      std::shared_ptr<ArrayVector> arrayVector;
+      if (auto iter = columnIndices_.find(path); iter != columnIndices_.end()) {
+        arrayVector =
+            std::dynamic_pointer_cast<ArrayVector>(vectors[iter->second]);
+      } else if (polymorphicTypeEnabled_) {
+        auto typedPath = getTypedColumnName(path, "varchar");
+        if (iter = columnIndices_.find(typedPath);
+            iter != columnIndices_.end()) {
+          arrayVector =
+              std::dynamic_pointer_cast<ArrayVector>(vectors[iter->second]);
+        }
+      } else {
+        break;
+      }
+      if (arrayOffsets_.find(path) == arrayOffsets_.end()) {
+        arrayOffsets_[path] = 0;
+      }
+      auto arrayBeginOffset = arrayOffsets_[path];
+      auto arrayEndOffset = arrayBeginOffset;
+      auto elements = arrayVector->elements();
+      for (auto arrayElement : element.get_array()) {
+        // Get each array element as a string
+        auto elementString = simdjson::to_json_string(arrayElement).value();
+        // Set the element in the array vector
+        elements->asFlatVector<StringView>()->set(
+            arrayEndOffset, StringView(elementString));
+        arrayEndOffset++;
+      }
+      arrayOffsets_[path] = arrayEndOffset;
+      arrayVector->setOffsetAndSize(
+          index, arrayBeginOffset, arrayEndOffset - arrayBeginOffset);
+      break;
+    }
+  }
+}
+} // namespace facebook::velox::connector::clp
--- velox/connectors/clp/ClpDataSource.h
+++ velox/connectors/clp/ClpDataSource.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <boost/process.hpp>
 #include "velox/connectors/Connector.h"
+#include "velox/connectors/clp/ClpConfig.h"
+
+#include "simdjson.h"
 
 namespace facebook::velox::connector::clp {
 class ClpDataSource : public DataSource {
@@ -11,10 +15,8 @@ class ClpDataSource : public DataSource {
       const std::unordered_map<
           std::string,
           std::shared_ptr<connector::ColumnHandle>>& columnHandles,
-      FileHandleFactory* fileHandleFactory,
-      folly::Executor* executor,
-      const ConnectorQueryCtx* connectorQueryCtx,
-      const std::shared_ptr<HiveConfig>& hiveConfig);
+      velox::memory::MemoryPool* pool,
+      std::shared_ptr<const ClpConfig>& clpConfig);
 
   void addSplit(std::shared_ptr<ConnectorSplit> split) override;
 
@@ -23,22 +25,43 @@ class ClpDataSource : public DataSource {
 
   void addDynamicFilter(
       column_index_t outputChannel,
-      const std::shared_ptr<common::Filter>& filter) override;
+      const std::shared_ptr<common::Filter>& filter) override {
+    VELOX_NYI("Dynamic filters not supported by ClpConnector.");
+  }
 
   uint64_t getCompletedBytes() override {
-    return ioStats_->rawBytesRead();
+    return completedBytes_;
   }
 
   uint64_t getCompletedRows() override {
     return completedRows_;
   }
 
-  std::unordered_map<std::string, RuntimeCounter> runtimeStats() override;
-
-  bool allPrefetchIssued() const override {
-    return splitReader_ && splitReader_->allPrefetchIssued();
+  std::unordered_map<std::string, RuntimeCounter> runtimeStats() override {
+    return {};
   }
 
-  void setFromDataSource(std::unique_ptr<DataSource> sourceUnique) override;
+ private:
+  static std::string getTypedColumnName(std::string& name, std::string suffix) {
+    return name + "_" + suffix;
+  }
+
+  void parseJsonLine(
+      simdjson::ondemand::value element,
+      std::string& path,
+      std::vector<VectorPtr>& vectors,
+      uint64_t index);
+
+  std::string executablePath_;
+  std::string archiveDir_;
+  std::string kqlQuery_;
+  bool polymorphicTypeEnabled_;
+  velox::memory::MemoryPool* pool_;
+  boost::process::ipstream resultsStream_;
+  RowTypePtr outputType_;
+  std::map<std::string, size_t> columnIndices_;
+  std::map<std::string, size_t> arrayOffsets_;
+  uint64_t completedRows_{0};
+  uint64_t completedBytes_{0};
 };
 } // namespace facebook::velox::connector::clp
