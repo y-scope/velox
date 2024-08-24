17f1a4ebc4d2dd31f7dc23440ba5434baabd289e
wraymo <raywangv@gmail.com>
2024-08-23 20:32:50 -0400

    add clp data source support

--- velox/connectors/clp/ClpTableHandle.h
+++ velox/connectors/clp/ClpTableHandle.h
@@ -5,11 +5,20 @@
 namespace facebook::velox::connector::clp {
 class ClpTableHandle : public ConnectorTableHandle {
  public:
-  ClpTableHandle(std::string connectorId, const std::string& tableName)
-      : ConnectorTableHandle(connectorId), tableName_(tableName) {}
+  ClpTableHandle(
+      std::string connectorId,
+      const std::string& tableName,
+      std::shared_ptr<std::string> query)
+      : ConnectorTableHandle(connectorId),
+        tableName_(tableName),
+        query_(std::move(query)) {}
 
   [[nodiscard]] const std::string& tableName() const {
     return tableName_;
+  }
+
+  [[nodiscard]] const std::shared_ptr<std::string>& query() const {
+    return query_;
   }
 
   std::string toString() const override;
@@ -18,5 +27,6 @@ class ClpTableHandle : public ConnectorTableHandle {
 
  private:
   const std::string tableName_;
+  std::shared_ptr<std::string> query_;
 };
 } // namespace facebook::velox::connector::clp
