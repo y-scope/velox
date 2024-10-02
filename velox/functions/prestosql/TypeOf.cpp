a76bf19972e20a9ab006d12305d4da2affba07a5
wraymo <raywangv@gmail.com>
2024-10-02 01:12:24 +0000

    apply changes from upstream

--- velox/functions/prestosql/TypeOf.cpp
+++ velox/functions/prestosql/TypeOf.cpp
@@ -84,6 +84,8 @@ std::string typeName(const TypePtr& type) {
     case TypeKind::VARBINARY:
       if (isHyperLogLogType(type)) {
         return "HyperLogLog";
+      } else if (isIPPrefixType(type)) {
+        return "ipprefix";
       }
       if (*type == *TDIGEST(DOUBLE())) {
         return "tdigest(double)";
