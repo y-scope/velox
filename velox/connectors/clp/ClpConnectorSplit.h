4c259b7ba0a23e9407502a13cd14ed00c9be9834
rwang22 <rwang22@ext.uber.com>
2025-03-17 17:29:25 +0000

    update velox code

--- velox/connectors/clp/ClpConnectorSplit.h
+++ velox/connectors/clp/ClpConnectorSplit.h
@@ -3,37 +3,23 @@
 #include "velox/connectors/Connector.h"
 
 namespace facebook::velox::connector::clp {
-class ClpConnectorSplit : public connector::ConnectorSplit {
- public:
+struct ClpConnectorSplit : public connector::ConnectorSplit {
   ClpConnectorSplit(
       const std::string& connectorId,
       const std::string& schemaName,
       const std::string& tableName,
-      const std::string& archiveId)
+      const std::string& archivePath)
       : connector::ConnectorSplit(connectorId),
         schemaName_(schemaName),
         tableName_(tableName),
-        archiveId_(archiveId) {}
+        archivePath_(archivePath) {}
 
   [[nodiscard]] std::string toString() const override {
     return fmt::format("CLP: {}.{}", schemaName_, tableName_);
   }
 
-  [[nodiscard]] std::string schemaName() const {
-    return schemaName_;
-  }
-
-  [[nodiscard]] std::string tableName() const {
-    return tableName_;
-  }
-
-  [[nodiscard]] std::string archiveId() const {
-    return archiveId_;
-  }
-
- private:
   const std::string schemaName_;
   const std::string tableName_;
-  const std::string archiveId_;
+  const std::string archivePath_;
 };
 } // namespace facebook::velox::connector::clp
