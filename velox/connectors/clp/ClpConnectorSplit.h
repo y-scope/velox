7c0f91eca5946c60935e7f5976e01f150838cee8
gibber9809 <devinbook1@gmail.com>
2025-04-10 21:50:26 +0000

    Update ClpConnectorSplit to include file-type info

--- velox/connectors/clp/ClpConnectorSplit.h
+++ velox/connectors/clp/ClpConnectorSplit.h
@@ -8,18 +8,23 @@ struct ClpConnectorSplit : public connector::ConnectorSplit {
       const std::string& connectorId,
       const std::string& schemaName,
       const std::string& tableName,
-      const std::string& archivePath)
+      const std::string& archivePath,
+      const int archiveType)
       : connector::ConnectorSplit(connectorId),
         schemaName_(schemaName),
         tableName_(tableName),
-        archivePath_(archivePath) {}
+        archivePath_(archivePath),
+        archiveType_(archiveType) {}
 
   [[nodiscard]] std::string toString() const override {
     return fmt::format("CLP: {}.{}", schemaName_, tableName_);
   }
 
+  enum class ArchiveType : int { kUnknown, kDefaultSFA, kIRV2 };
+
   const std::string schemaName_;
   const std::string tableName_;
   const std::string archivePath_;
+  const int archiveType_;
 };
 } // namespace facebook::velox::connector::clp
