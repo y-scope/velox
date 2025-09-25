ae977665c7f748431f9c95a8a9ac50263d926309
anlowee <anlowee@outlook.com>
2025-09-25 20:16:45 +0000

    Address comment

--- velox/connectors/clp/search_lib/ir/ClpIrCursor.cpp
+++ velox/connectors/clp/search_lib/ir/ClpIrCursor.cpp
@@ -18,7 +18,6 @@
 #include "clp_s/InputConfig.hpp"
 
 #include "ffi/ir_stream/search/QueryHandler.hpp"
-#include "velox/connectors/clp/search_lib/ClpTimestampsUtils.h"
 #include "velox/connectors/clp/search_lib/ir/ClpIrCursor.h"
 #include "velox/connectors/clp/search_lib/ir/ClpIrVectorLoader.h"
 
