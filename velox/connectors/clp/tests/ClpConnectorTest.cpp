2ba6894699400c8a21dfbd418380e3e7b28d6fd3
gibber9809 <devinbook1@gmail.com>
2026-02-18 20:25:07 +0000

    Update out of date query.

--- velox/connectors/clp/tests/ClpConnectorTest.cpp
+++ velox/connectors/clp/tests/ClpConnectorTest.cpp
@@ -663,7 +663,7 @@ TEST_F(ClpConnectorTest, test5FloatTimestampPushdown) {
   // Test filtering rows with a timestamp parsed from a date string and floats
   // in various formats.
   const std::shared_ptr<std::string> kqlQuery = std::make_shared<std::string>(
-      R"(timestamp < timestamp("1746003005.127") and timestamp >= timestamp("1746003005.124")");
+      R"(timestamp < timestamp("1746003070000", "\L") and timestamp >= timestamp("1746003005124", "\L")");
   auto plan =
       PlanBuilder(pool_.get())
           .startTableScan()
