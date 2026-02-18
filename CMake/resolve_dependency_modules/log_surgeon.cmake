c78f992f7606e2dba3648c912a916d592d901d32
gibber9809 <devinbook1@gmail.com>
2026-02-18 21:10:32 +0000

    Update log-surgeon dependency to match version currently used in clp

--- CMake/resolve_dependency_modules/log_surgeon.cmake
+++ CMake/resolve_dependency_modules/log_surgeon.cmake
@@ -16,7 +16,7 @@ include_guard(GLOBAL)
 FetchContent_Declare(
   log_surgeon
   GIT_REPOSITORY https://github.com/y-scope/log-surgeon.git
-  GIT_TAG 85d4f2c09c0e55f1fb87cdc8b0f4d13fb1a733e1
+  GIT_TAG eebf857962f8fc022bfa5de8d00e10ee4ac36bb8
   OVERRIDE_FIND_PACKAGE)
 
 set(log_surgeon_BUILD_TESTING OFF)
